dee8438fcd15869fb9841e8971e14e9856a71045
Mitar <mitar.git@tnode.com>
2018-01-23 05:23:20 -0800

    First check the code.

--- code/backup.py
+++ code/backup.py
@@ -13,12 +13,12 @@ with request.urlopen('https://podcrto.si/feed/') as feed_response:
     for link in tree.getroot().findall('./channel/item/link'):
         url = link.text
         with request.urlopen('https://web.archive.org/save/' + url) as archive_response:
-            # Read in the whole page to be a good client.
-            archive_response.read()
-
             if archive_response.getcode() != 200:
                 print("Archiving '{url}' failed.".format(url=url))
                 failed = True
+
+            # Read in the whole page to be a good client.
+            archive_response.read()
 
         # Sleep a bit.
         time.sleep(5)
