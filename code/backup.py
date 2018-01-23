12885a0f80af884666c72b5b0436f9f69fddf751
Mitar <mitar.git@tnode.com>
2018-01-23 13:02:56 -0800

    Also archive the front page.

--- code/backup.py
+++ code/backup.py
@@ -7,18 +7,26 @@ from xml.etree import ElementTree as etree
 
 failed = False
 
+
+def archive(url):
+    global failed
+
+    with request.urlopen('https://web.archive.org/save/' + url) as archive_response:
+        if archive_response.getcode() != 200:
+            print("Archiving '{url}' failed.".format(url=url))
+            failed = True
+
+        # Read in the whole page to be a good client.
+        archive_response.read()
+
+
+archive('https://podcrto.si')
+
 with request.urlopen('https://podcrto.si/feed/') as feed_response:
     tree = etree.parse(feed_response)
 
     for link in tree.getroot().findall('./channel/item/link'):
-        url = link.text
-        with request.urlopen('https://web.archive.org/save/' + url) as archive_response:
-            if archive_response.getcode() != 200:
-                print("Archiving '{url}' failed.".format(url=url))
-                failed = True
-
-            # Read in the whole page to be a good client.
-            archive_response.read()
+        archive(link.text)
 
         # Sleep a bit.
         time.sleep(5)
